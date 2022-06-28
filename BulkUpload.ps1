b4308ad3b4de58349ae6a5f6073dcff9ed49680a
ShmuelBH <108005832+ShmuelBH@users.noreply.github.com>
2022-06-28 15:50:55 +0300

    Update BulkUpload.ps1

--- BulkUpload.ps1
+++ BulkUpload.ps1
@@ -1,33 +1,68 @@
-Import-Module Sharegate
-$Location = Split-Path $psISE.CurrentFile.FullPath
-Push-Location
+#$Path = "D:\PenangCommon\SHE" ; Get-ChildItem -Directory  -path $Path | Select-Object {$_.FullName} #| Out-File ".\List.csv"
+#Import-Module Sharegate #Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+#Clear-Variable dstSite, dstList -ErrorAction Ignore
+$ExportReport = "off" #Export-Report to Excell
+$Timer = "off"
+if ($Timer -eq 'on') {
+    $current = Get-Date
+    $end = Get-Date -Month 06 -Day 28 -Year 2022 -Hour 19 -Minute 00
+    $diff= New-TimeSpan -Start $current -End $end
+    Write-Host Current Time and date: $current, End Time $end ,Timer is set to $diff
+    Start-Sleep -s $diff.TotalSeconds
+}
+
+#User&Pass - Migration Account 1
+$myuser = 'Mail'
+$mypassword = ConvertTo-SecureString 'Pass' -AsPlainText -Force
+
+#CSV
+$Location = Split-Path $psISE.CurrentFile.FullPath ; Push-Location $Location
 $csvFile = $Location+'\BulkUpload.csv'
 $table = Import-Csv $csvFile -Delimiter ","
-$mypassword = ConvertTo-SecureString 'pass' -AsPlainText -Force
-$myuser = 'user'
+
+#Parameters
+#$TimeRangeFilter = (Get-Date).AddDays(-40)
+#$propertyTemplate = New-PropertyTemplate -VersionHistory -VersionLimit 1 -AuthorsAndTimestamps SameAsCurrent -Permissions Ignore -From $TimeRangeFilter
 $copysettings = New-CopySettings -OnContentItemExists IncrementalUpdate
 $propertyTemplate = New-PropertyTemplate -AuthorsAndTimestamps
+
 Set-Variable dstSite, dstList, dstLib
 foreach ($row in $table)
 {
     if ($row.Status -notmatch "Upload")
     {
-        Clear-Variable dstSite
-        Clear-Variable dstList
-        Clear-Variable dstLib
+        #Start Time
+        $StartTime = Get-date #"{0:G}" -f (Get-date)
+        Write-Host Start Time: $StartTime
+        $LogTime = (Get-Date (Get-Date ).AddHours(-24) -format "dd-MM_HH.mm.ss")
+
+        Write-Host $row.SourcePath
+        Write-Host $row.DestinationPath
+
+        Clear-Variable dstSite, dstList, dstLib -ErrorAction Ignore
         $dstLib = Split-Path $row.DestinationPath -Leaf
         $dstSite = Connect-Site -Url $row.DestinationPath -UserName $myuser -Password $mypassword -AllowConnectionFallback -WarningAction Ignore
-        $ID = Get-List -Site $dstSite | Where-Object -Property Address -match $dstLib | Select-Object Id -First 1
+        $ID = Get-List -Site $dstSite | Where-Object -Property RootFolder -match $dstLib | Select-Object Id -First 1
         $dstList = Get-List -Site $dstSite -Id $ID.Id 
-        Import-Document -SourceFolder $row.SourcePath -DestinationList $dstList -CopySettings $copysettings -Template $propertyTemplate -TaskName $row.SourcePath
+        $result = Import-Document -SourceFolder $row.SourcePath -DestinationList $dstList -CopySettings $copysettings -Template $propertyTemplate -TaskName $row.SourcePath -WarningAction Ignore
+        $result
+        Write-Host 
         Write-Output $dstList.Address.AbsoluteUri
         $row.Status = 'Upload'+$row.Status
         $table | Export-Csv ./BulkUpload.csv -Delimiter ',' -NoType
+
+        #Time Elapsed
+        $FinishTime = Get-date #"{0:G}" -f (Get-date)
+        Write-Host Start Time: $StartTime 
+        Write-Host End Time: $FinishTime
+        Write-Host "Time elapsed: $(New-Timespan $StartTime $FinishTime)" -f White -b cyan `n
+
+        #Log
+        if ($ExportReport -eq 'on') { Export-Report -CopyResult $result -Path ".\$dstLib-$LogTime.xlsx" } #-DefaultColumns
     }
     else
     {
-        Write-Host 'Skipping:' -ForegroundColor Cyan
-        Write-Host $row.DestinationPath
-        continue
+    Write-Host
+    Write-Host $row.DestinationPath - Allredy Copy -ForegroundColor Cyan
     }
 }

8354b22cf2d4e3cd48a30088cf4650244b431342
Chodzz <102915201+Chodzz@users.noreply.github.com>
2022-06-28 12:11:44 +0300

    Update BulkUpload.ps1

--- BulkUpload.ps1
+++ BulkUpload.ps1
@@ -3,7 +3,7 @@ $Location = Split-Path $psISE.CurrentFile.FullPath
 Push-Location
 $csvFile = $Location+'\BulkUpload.csv'
 $table = Import-Csv $csvFile -Delimiter ","
-$mypassword = ConvertTo-SecureString 'pw' -AsPlainText -Force
+$mypassword = ConvertTo-SecureString 'pass' -AsPlainText -Force
 $myuser = 'user'
 $copysettings = New-CopySettings -OnContentItemExists IncrementalUpdate
 $propertyTemplate = New-PropertyTemplate -AuthorsAndTimestamps
@@ -16,10 +16,11 @@ foreach ($row in $table)
         Clear-Variable dstList
         Clear-Variable dstLib
         $dstLib = Split-Path $row.DestinationPath -Leaf
-        $dstSite = Connect-Site -Url $row.DestinationPath -UserName $myuser -Password $mypassword -AllowConnectionFallback
-        $dstList = Get-List -Name $dstLib -Site $dstSite
-        Write-Output $dstList
-        Import-Document -SourceFolder $row.SourcePath -DestinationList $dstList -CopySettings $copysettings -Template $propertyTemplate -TaskName $row.SourcePath -WaitForImportCompletion
+        $dstSite = Connect-Site -Url $row.DestinationPath -UserName $myuser -Password $mypassword -AllowConnectionFallback -WarningAction Ignore
+        $ID = Get-List -Site $dstSite | Where-Object -Property Address -match $dstLib | Select-Object Id
+        $dstList = Get-List -Site $dstSite -Id $ID.Id 
+        Import-Document -SourceFolder $row.SourcePath -DestinationList $dstList -CopySettings $copysettings -Template $propertyTemplate -TaskName $row.SourcePath
+        Write-Output $dstList.Address.AbsoluteUri
         $row.Status = 'Upload'+$row.Status
         $table | Export-Csv ./BulkUpload.csv -Delimiter ',' -NoType
     }
